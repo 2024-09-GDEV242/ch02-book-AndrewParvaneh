6619c3e501f7ae39b2dc72ac8f7a89af9c45f513
Andrew Parvaneh <123667168+AndrewParvaneh@users.noreply.github.com>
2024-09-17 16:45:37 -0400

    Update Book.java

--- Book.java
+++ Book.java
@@ -11,15 +11,19 @@ class Book
     // The fields.
     private String author;
     private String title;
+    private int pages;
+    private String refNumber;
 
     /**
-     * Set the author and title fields when this object
-     * is constructed.
+     * Set the author, title, pages fields and initialize the reference number 
+     * when this object is constructed.
      */
-    public Book(String bookAuthor, String bookTitle)
+    public Book(String bookAuthor, String bookTitle, int bookPages)
     {
         author = bookAuthor;
         title = bookTitle;
+        pages = bookPages;
+        refNumber = "";
     }
     
     /**
@@ -41,6 +45,15 @@ class Book
     }
     
     /**
+     * This method satisfies the requirement of problem 2.85
+     * Returns the number of pages in the book.
+     */
+    public int getPages()
+    {
+        return pages;
+    }
+    
+    /**
      * This method satisfies the requirement of problem 2.84
      * Prints the author of the book to the terminal.
      */
@@ -56,5 +69,32 @@ class Book
     public void printTitle()
     {
         System.out.println("Title: " + title);
+    }
+    
+    /**
+     * This method satisfies the requirement of problem 2.87
+     * Prints the details of the book to the terminal.
+     */
+    public void printDetails()
+    {
+        System.out.println("Title: " + title + ", Author: " + author + ", Pages: " + pages);
+    }
+    
+    /**      
+    * This method sets the reference number for the book.
+    * Satisfies the requirement of problem 2.88
+    */
+    public void setRefNumber(String ref)
+    {
+        refNumber = ref;
+    }
+    
+    /**      
+    * This method returns the reference number of the book.
+    * Satisfies the requirement of problem 2.88
+    */
+    public String getRefNumber()
+    {
+        return refNumber;
     }
 }
